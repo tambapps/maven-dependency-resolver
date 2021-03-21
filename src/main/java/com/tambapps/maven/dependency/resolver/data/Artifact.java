95f3888f751fa971ffc65663678a1be500ed1a99
Nelson <nfonkoua@weborama.com>
2021-03-21 23:39:44 +0100

    initialized dependency version with parent dependency managment when found

--- src/main/java/com/tambapps/maven/dependency/resolver/data/Artifact.java
+++ src/main/java/com/tambapps/maven/dependency/resolver/data/Artifact.java
@@ -2,7 +2,10 @@ package com.tambapps.maven.dependency.resolver.data;
 
 import lombok.Data;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Data
 public class Artifact {
@@ -26,5 +29,24 @@ public class Artifact {
   @Override
   public String toString() {
     return String.join(":", groupId, artifactId, version);
+  }
+
+  public void setParent(Artifact parent) {
+    this.parent = parent;
+    // look for dependency version if some is not specified
+    List<Dependency> depsWithoutVersion = dependencies.stream().filter(d -> d.getVersion() == null)
+        .collect(Collectors.toList());
+    Artifact parentArtifact = parent;
+    while (depsWithoutVersion.size() > 0 && parentArtifact != null) {
+      Iterator<Dependency> iterator = depsWithoutVersion.iterator();
+      while (iterator.hasNext()) {
+        Dependency dependency = iterator.next();
+        Optional<Dependency> optMatchedDependencyManagement =
+            parentArtifact.getDependencyManagement().stream().filter(dependency::matches).findFirst();
+        optMatchedDependencyManagement.ifPresent(value -> dependency.setVersion(value.getVersion()));
+        iterator.remove();
+      }
+      parentArtifact = parentArtifact.getParent();
+    }
   }
 }
