d9958e2cf83b463f8c94d86d055c8fa385bc13cd
Nelson <nfonkoua@weborama.com>
2021-03-21 23:20:26 +0100

    handled case when groupId is defined in parent

--- src/main/java/com/tambapps/maven/dependency/resolver/repository/AbstractMavenRepository.java
+++ src/main/java/com/tambapps/maven/dependency/resolver/repository/AbstractMavenRepository.java
@@ -4,6 +4,7 @@ import com.tambapps.maven.dependency.resolver.data.Artifact;
 import com.tambapps.maven.dependency.resolver.data.Dependency;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -41,7 +42,25 @@ public abstract class AbstractMavenRepository implements MavenRepository {
     document.getElementsByTagName("dependencies");
     Artifact artifact = new Artifact();
     // TODO handle case where pom has a parent and groupId is defined in parent
-    artifact.setGroupId(document.getElementsByTagName("groupId").item(0).getTextContent());
+    //  only fetch parent when needed (meaning when a version of a dependency isn't specified
+
+    // TODO sometimes groupId is only defined in parent:
+    /*
+    <parent>
+    <groupId>com.tambapps.gmage</groupId>
+    <artifactId>gmage</artifactId>
+    <version>1.0-SNAPSHOT</version>
+  </parent>
+  <artifactId>gmage-desktop</artifactId>
+     */
+    String groupId = getPropertyOrNull(document, "groupId");
+    Element parentNode = getElementOrNull(document, "parent");
+
+    if (groupId == null && parentNode != null) {
+      groupId = getPropertyOrNull(parentNode, "groupId");
+    }
+
+    artifact.setGroupId(groupId);
     artifact.setArtifactId(document.getElementsByTagName("artifactId").item(0).getTextContent());
     artifact.setVersion(document.getElementsByTagName("version").item(0).getTextContent());
     artifact.setDependencies(extractDependencies(document.getElementsByTagName("dependencies")));
@@ -49,17 +68,29 @@ public abstract class AbstractMavenRepository implements MavenRepository {
     return artifact;
   }
 
+  private Element getElementOrNull(Document document, String tagName) {
+    NodeList nodes = document.getElementsByTagName(tagName);
+    return nodes.getLength() == 0 ? null : (Element) nodes.item(0);
+  }
+
+  private String getPropertyOrNull(Document document, String tagName) {
+    NodeList nodes = document.getElementsByTagName(tagName);
+    return nodes.getLength() == 0 ? null :  nodes.item(0).getTextContent();
+  }
+
+  private String getPropertyOrNull(Element document, String tagName) {
+    NodeList nodes = document.getElementsByTagName(tagName);
+    return nodes.getLength() == 0 ? null :  nodes.item(0).getTextContent();
+  }
   private List<Dependency> extractDependencies(NodeList dependencyNodes) {
     List<Dependency> dependencies = new ArrayList<>();
 
     for (int i = 0; i < dependencyNodes.getLength(); i++) {
       Element node = (Element) dependencyNodes.item(i);
-      NodeList versionNodes = node.getElementsByTagName("version");
       dependencies.add(Dependency.builder()
           .groupId(node.getElementsByTagName("groupId").item(0).getTextContent())
           .artifactId(node.getElementsByTagName("artifactId").item(0).getTextContent())
-          .version(versionNodes.getLength() == 0 ? null : node.getElementsByTagName("version")
-              .item(0).getTextContent())
+          .version(getPropertyOrNull(node, "version"))
           .build());
     }
     return dependencies;
