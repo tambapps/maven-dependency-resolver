6373fa1b0d4ae872e68a145a8353589b97a1f3f7
Nelson <nfonkoua@weborama.com>
2021-03-22 20:19:32 +0100

    fixed (another) bug

--- src/main/java/com/tambapps/maven/dependency/resolver/repository/RemoteMavenRepository.java
+++ src/main/java/com/tambapps/maven/dependency/resolver/repository/RemoteMavenRepository.java
@@ -70,7 +70,7 @@ public class RemoteMavenRepository extends AbstractMavenRepository {
   @Override
   public Artifact retrieveArtifact(String groupId, String artifactId, String version)
       throws IOException {
-    Request request = jarRequest(groupId, artifactId, version).get().build();
+    Request request = pomRequest(groupId, artifactId, version).get().build();
     Response response = client.newCall(request).execute();
     if (!response.isSuccessful()) {
       throw new IOException(String.format("Requesting artifact %s:%s:%s failed: %s",groupId, artifactId, version, response.message()));
