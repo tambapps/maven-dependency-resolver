8dd6838a3feb8ef88af4e1eaaac5f04ee75cd47f
Tambapps <tambapps@gmail.com>
2021-10-12 23:39:27 +0200

    removed old dependency resolver

--- src/main/java/com/tambapps/maven/dependency/resolver/DependencyResolver.java
+++ src/main/java/com/tambapps/maven/dependency/resolver/DependencyResolver.java
@@ -1,75 +1,90 @@
 package com.tambapps.maven.dependency.resolver;
 
-
-import com.tambapps.maven.dependency.resolver.data.DependencyResolvingResult;
-import com.tambapps.maven.dependency.resolver.data.PomArtifact;
 import com.tambapps.maven.dependency.resolver.data.Artifact;
 import com.tambapps.maven.dependency.resolver.data.Dependency;
+import com.tambapps.maven.dependency.resolver.data.DependencyResolvingResult;
+import com.tambapps.maven.dependency.resolver.data.PomArtifact;
 import com.tambapps.maven.dependency.resolver.data.Scope;
 import com.tambapps.maven.dependency.resolver.repository.MavenRepository;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-public final class DependencyResolver {
+/**
+ * Class allowing to fetch an artifact along with its transitive dependencies.
+ * This class is definitely NOT thread-safe
+ */
+public class DependencyResolver {
 
-  private DependencyResolver() {}
+  private final MavenRepository repository;
+  private final List<Artifact> fetchedArtifacts = new ArrayList<>();
 
-  public static DependencyResolvingResult resolve(MavenRepository repository, String dependencyString)
-      throws IOException {
-    String[] fields = Artifact.extractFields(dependencyString);
-    return resolve(repository, fields[0], fields[1], fields[2]);
+  public DependencyResolver(MavenRepository repository) {
+    this.repository = repository;
   }
 
-  public static DependencyResolvingResult resolve(MavenRepository repository, PomArtifact pomArtifact)
-      throws IOException {
-    return resolve(repository, pomArtifact.getGroupId(), pomArtifact.getArtifactId(), pomArtifact.getVersion());
+  public DependencyResolvingResult resolve(Artifact artifact) throws IOException {
+    return resolve(artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion());
   }
+  public DependencyResolvingResult resolve(String groupId, String artifactId, String version) throws IOException {
+    // reset variables in case resolve(...) has already been called
+    fetchedArtifacts.clear();
 
-  public static DependencyResolvingResult resolve(MavenRepository repository, String groupId, String artifactId, String version)
-      throws IOException {
-    return resolve(repository, Collections.singletonList(new Artifact(groupId, artifactId, version)));
-  }
+    PomArtifact pomArtifact = repository.retrieveArtifact(groupId, artifactId, version);
 
-  public static DependencyResolvingResult resolve(MavenRepository repository, List<Artifact> artifacts)
-      throws IOException {
-    List<Artifact> dependencies = new ArrayList<>();
-    Set<Artifact> visitedArtifacts = new HashSet<>();
-    for (Artifact artifact : artifacts) {
-      resolveRec(repository, visitedArtifacts, dependencies, artifact.toBase());
-    }
-    Map<String, List<Artifact>> artifactVersionsMap = new HashMap<>();
-    for (Artifact artifact : dependencies) {
-      artifactVersionsMap.computeIfAbsent(String.format("%s:%s", artifact.getGroupId(), artifact.getArtifactId()), k -> new ArrayList<>())
-          .add(artifact);
-    }
-    return new DependencyResolvingResult(dependencies, artifactVersionsMap);
-  }
-
-  private static void resolveRec(MavenRepository repository,
-      Set<Artifact> visitedArtifacts, List<Artifact> dependencies, Artifact artifactDependency)
-      throws IOException {
-    if (visitedArtifacts.contains(artifactDependency)) {
-      return;
-    }
-    PomArtifact pomArtifact = repository.retrieveArtifact(artifactDependency.getGroupId(),
-        artifactDependency.getArtifactId(), artifactDependency.getVersion());
-    Artifact artifact = pomArtifact.toBase();
-    dependencies.add(artifact);
-    visitedArtifacts.add(artifact);
-    // TODO also fetch parent dependencies if any
     for (Dependency dependency : pomArtifact.getDependencies()) {
-      if (dependency.isOptional() || dependency.getScope() != Scope.COMPILE) {
+      // here we gooooooooo
+      List<Dependency> dependencyPath = new ArrayList<>();
+      if (shouldSkip(dependency, dependencyPath)) {
         continue;
       }
-      resolveRec(repository, visitedArtifacts, dependencies, dependency);
+      resolveRec(dependency, dependencyPath);
     }
+
+    // map groupId:artifactId -> versions
+    Map<String, List<Artifact>> artifactVersionsMap = new HashMap<>();
+
+    for (Artifact artifact : fetchedArtifacts) {
+      artifactVersionsMap.computeIfAbsent(artifact.getGroupId() + ":" + artifact.getArtifactId(), k -> new ArrayList<>())
+          .add(artifact);
+    }
+    return new DependencyResolvingResult(fetchedArtifacts, artifactVersionsMap);
+  }
+
+  /**
+   * Resolve recursively the dependencies
+   *
+   * @param dependency     the dependency to resolve
+   * @param dependencyPath the dependency path, allowing to track in the dependency graph the parent dependencies of the one we're fetching. (Used to check exclusion)
+   * @throws IOException in case of I/O error
+   */
+  private void resolveRec(Dependency dependency, List<Dependency> dependencyPath) throws IOException {
+    PomArtifact pomArtifact = repository.retrieveArtifact(dependency);
+    fetchedArtifacts.add(pomArtifact);
+
+    for (Dependency artifactDependency : pomArtifact.getDependencies()) {
+      if (shouldSkip(artifactDependency, dependencyPath)) {
+        continue;
+      }
+      List<Dependency> childDependencyBranch = new ArrayList<>(dependencyPath);
+      childDependencyBranch.add(dependency);
+      resolveRec(artifactDependency, childDependencyBranch);
+    }
+  }
+
+  private boolean shouldSkip(Dependency artifactDependency, List<Dependency> dependencyPath) {
+    return artifactDependency.isOptional() ||
+        artifactDependency.getScope() != Scope.COMPILE ||
+        fetchedArtifacts.stream().anyMatch(artifactDependency::matches) ||
+        isExcluded(artifactDependency, dependencyPath);
+  }
+
+  private boolean isExcluded(Dependency dependency, List<Dependency> dependencyBranch) {
+    return dependencyBranch.stream()
+        .anyMatch(db -> db.isAnyArtifact() || db.matches(dependency));
   }
 
 }
